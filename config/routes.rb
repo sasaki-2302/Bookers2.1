f3932516696fd40fc7ef9a77d06d38e89e5615a6
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-02-23 02:43:38 +0000

    Book_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 end
