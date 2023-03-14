be237c7645f2cb8f2a39684048e7b31d46c1beea
sasaki-2302 <124408816+sasaki-2302@users.noreply.github.com>
2023-03-14 01:04:04 +0000

    create_relationship_controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   end
   root to: "homes#top"
   get "home/about" => "homes#about"
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
